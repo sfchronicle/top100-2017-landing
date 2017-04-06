b15879113497e81f27ea5557e9fa1dd331b407f8
emma <emmaruthoneill@gmail.com>
2017-04-06 12:20:13 -0700

    put in buttons

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,16 @@ require("./lib/social"); //Do not delete
 console.log(textData);
 console.log(mapData);
 console.log(photoData);
+
+var len = Object.keys(textData).length;
+var keys = Object.keys(textData);
+
+for (var idx=0; idx<len; idx++){
+  console.log(keys[idx]);
+  console.log(textData[keys[idx]]);
+  photoData.forEach(function(photo) {
+    if (photo.id == keys[idx]) {
+
+  });
+
+}
