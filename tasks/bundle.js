ffc222c9f6243f148c7bef60d7bc1247207c37cd
emma <emmaruthoneill@gmail.com>
2017-04-20 18:38:56 -0700

    got rid of map files which we dont need

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -16,8 +16,7 @@ module.exports = function(grunt) {
 
     //specify starter files here - if you need additionally built JS, just add it.
     var seeds = {
-      "./src/js/main.js": "build/app.js",
-      "./src/js/map.js": "build/map-app.js"
+      "./src/js/main.js": "build/app.js"
     };
 
     async.forEachOf(seeds, function(dest, src, c) {
